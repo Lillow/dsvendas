29fe156fec8e8a9ef0ff1a558623d0671798a025
Danillo Silva <danillordm@gmail.com>
2021-11-02 10:08:49 -0300

    Small change.

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -13,10 +13,10 @@ const DataTable = () => {
         </thead>
         <tbody>
           <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
+            <td>11/01/2021</td>
+            <td>Anakin</td>
+            <td>28</td>
+            <td>10</td>
             <td>15017.00</td>
           </tr>
           <tr>
@@ -27,59 +27,24 @@ const DataTable = () => {
             <td>15017.00</td>
           </tr>
           <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
+            <td>19/06/2021</td>
+            <td>Kal-El</td>
+            <td>65</td>
+            <td>55</td>
             <td>15017.00</td>
           </tr>
           <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
+            <td>25/10/2021</td>
+            <td>Logan</td>
+            <td>23</td>
+            <td>9</td>
             <td>15017.00</td>
           </tr>
           <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
+            <td>30/03/2021</td>
+            <td>Padmé</td>
+            <td>54</td>
+            <td>49</td>
             <td>15017.00</td>
           </tr>
         </tbody>
