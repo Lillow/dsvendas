5af22f4408cfed6cc180430cc7a7486f53f8e59c
Danillo Silva <danillordm@gmail.com>
2021-11-06 23:56:39 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,26 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { SalePage } from 'types/sale'
+import { formatLocalDate } from 'utils/format'
+import { BASE_URL } from 'utils/requests'
+
 const DataTable = () => {
+  const [page, setPage] = useState<SalePage>({
+    first: true,
+    last: true,
+    number: 0,
+    totalElements: 0,
+    totalPages: 0
+  })
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+      .then(response => {
+        setPage(response.data)
+      })
+  }, [])
+
   return (
     <div className="table-responsive">
       <table className="table table-striped table-sm">
@@ -12,41 +34,15 @@ const DataTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>11/01/2021</td>
-            <td>Anakin</td>
-            <td>28</td>
-            <td>10</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>19/06/2021</td>
-            <td>Kal-El</td>
-            <td>65</td>
-            <td>55</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>25/10/2021</td>
-            <td>Logan</td>
-            <td>23</td>
-            <td>9</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>30/03/2021</td>
-            <td>Padmé</td>
-            <td>54</td>
-            <td>49</td>
-            <td>15017.00</td>
-          </tr>
+          {page.content?.map(item => (
+            <tr key={item.id}>
+              <td>{formatLocalDate(item.date, 'dd/MM/yyyy')}</td>
+              <td>{item.seller.name}</td>
+              <td>{item.visited}</td>
+              <td>{item.deals}</td>
+              <td>{item.amount.toFixed(2)}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
